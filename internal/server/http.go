3da1be1daf9cb6f25bf4fb44840c0d632ec30a86
abing <410174833@qq.com>
2023-09-27 11:34:48 +0800

    更换ipfs

--- internal/server/http.go
+++ internal/server/http.go
@@ -1,7 +1,9 @@
 package server
 
 import (
-	"context"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/transport/http"
 	"github.com/go-kratos/swagger-api/openapiv2"
 	computev1 "github.com/mohaijiang/computeshare-client/api/compute/v1"
 	v1 "github.com/mohaijiang/computeshare-client/api/helloworld/v1"
@@ -9,11 +11,6 @@ import (
 	"github.com/mohaijiang/computeshare-client/internal/conf"
 	"github.com/mohaijiang/computeshare-client/internal/service"
 	"github.com/mohaijiang/computeshare-client/third_party/agent"
-	"strings"
-
-	"github.com/go-kratos/kratos/v2/log"
-	"github.com/go-kratos/kratos/v2/middleware/recovery"
-	"github.com/go-kratos/kratos/v2/transport/http"
 )
 
 // NewHTTPServer new an HTTP server.
@@ -50,18 +47,18 @@ func NewHTTPServer(c *conf.Server,
 	srv.HandleFunc("/v1/vm/{id}/terminal", vmWebsocketHandler.Terminal)
 
 	// register
-	err := agentService.Register()
-	if err != nil {
-		panic(err)
-	}
+	//err := agentService.Register()
+	//if err != nil {
+	//	panic(err)
+	//}
 
-	port := strings.Split(c.Http.Addr, ":")[1]
-	_, err = p2pService.CreateListen(context.Background(), &p2pv1.CreateListenRequest{
-		Protocol:      "/x/ssh",
-		TargetAddress: "/ip4/127.0.0.1/tcp/" + port,
-	})
-	if err != nil {
-		panic(err)
-	}
+	//port := strings.Split(c.Http.Addr, ":")[1]
+	//_, err = p2pService.CreateListen(context.Background(), &p2pv1.CreateListenRequest{
+	//	Protocol:      "/x/ssh",
+	//	TargetAddress: "/ip4/127.0.0.1/tcp/" + port,
+	//})
+	//if err != nil {
+	//	panic(err)
+	//}
 	return srv
 }
